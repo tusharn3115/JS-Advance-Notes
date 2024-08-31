9e1cba67a1be45fc7afa34f69129275c3eb372fd
Tushar Negi <negitushar81@gmail.com>
2024-08-31 11:03:19 +0530

    .

--- arrays.js
+++ arrays.js
@@ -1,1 +1,4 @@
 // arrays ----------------------------------------------------------------------
+// it stores a collection of multiple items of different datatype under a single variable
+// these data are stored into a square brackets []
+const myArr = [0,1,2,3,4,5];
