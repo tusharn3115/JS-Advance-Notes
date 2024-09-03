0af324fd7d6f650caf332e4974144c7ba8413832
Tushar Negi <negitushar81@gmail.com>
2024-09-03 12:31:19 +0530

    .

--- dom.js
+++ dom.js
@@ -1,8 +1,13 @@
 const week = document.querySelector(".week");
-console.log(week);
-console.log(week.children)
-console.log(week.children[1].innerHTML);
+// console.log(week);
+// console.log(week.children)
+// console.log(week.children[1].innerHTML);
 
-for(let i = 0;i < week.children.length;i++){
-    console.log(week.children[i].innerHTML);
-}
+// for(let i = 0;i < week.children.length;i++){
+//     console.log(week.children[i].innerHTML);
+// }
+
+
+const days = document.querySelector(".days");
+// console.log(days);
+console.log(days.parentElement);
