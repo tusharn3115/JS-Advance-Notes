1e5f5ba94bac3658415ca6eba02c67cb5d0a93f2
Tushar Negi <negitushar81@gmail.com>
2024-09-03 12:30:01 +0530

    .

---
+++ dom.js
@@ -0,0 +1,8 @@
+const week = document.querySelector(".week");
+console.log(week);
+console.log(week.children)
+console.log(week.children[1].innerHTML);
+
+for(let i = 0;i < week.children.length;i++){
+    console.log(week.children[i].innerHTML);
+}
