fa62da4ea92ac322882f73693fadfd299d945efc
Tushar Negi <negitushar81@gmail.com>
2024-09-03 19:02:03 +0530

    .

--- DOMproperties.js
+++ DOMproperties.js
@@ -1,6 +1,16 @@
 // innerHTML vs innerText vs textContent
 const element = document.querySelector("h1");
+// hi has the value <h1>Beginner To Advance JS</h1>
+
 
 // 1. innerHTML
-const usingInnerHTML = element.innerHTML;
-console.log(usingInnerHTML);
+// it represents the HTML content inside an element and also the HTML tags used inside the element
+// const usingInnerHTML = element.innerHTML;
+// console.log(usingInnerHTML);  
+
+
+// 2. innerText
+// it only represents text content of an element
+// displays plain text and no HTML tags
+const usingInnerText = element.innerText;
+console.log(usingInnerText);
