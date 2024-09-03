40e8ea4a1f87f03af8b48500c64888df3b0e84d6
Tushar Negi <negitushar81@gmail.com>
2024-09-03 18:20:47 +0530

    .

--- dom.js
+++ dom.js
@@ -28,3 +28,29 @@ div.style.padding = "25px";
 const text = document.createTextNode("Hello form js");
 div.appendChild(text);  // appendChild is adding text to div
 // document.body.appendChild(div);  // showing our div to browser web page by using appendChild to body
+
+
+
+// Edit and remove elements in DOM -----------------------------------------------------------
+
+// adding element but this process is not optimized because it has to traverse the DOM tree
+function addLanguage(langName){
+    const li = document.createElement("li");
+    li.innerHTML = `${langName}`;  // using innerHTML is traverse the DOM tree
+    document.querySelector(".language").appendChild(li);
+}
+addLanguage("python");
+addLanguage("typescript");
+
+
+// optimized way to add an element
+function addOptiLanguage(langName){
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(langName));
+    document.querySelector('.language').appendChild(li);
+}
+addOptiLanguage("");
+addOptiLanguage("goLang");
+
+// edit element ------------------
+const editLang = document.querySelector("li:nth-child()")
