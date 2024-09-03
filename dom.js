02171e119f91fa679f66f8d848e0209f70edfbf4
Tushar Negi <negitushar81@gmail.com>
2024-09-03 12:45:47 +0530

    .

--- dom.js
+++ dom.js
@@ -1,3 +1,5 @@
+// parent , children , nextElementSibling --------------------------------------- 
+
 const week = document.querySelector(".week");
 // console.log(week);
 // console.log(week.children)
@@ -11,4 +13,18 @@ const week = document.querySelector(".week");
 const days = document.querySelector(".days");
 // console.log(days);
 // console.log(days.parentElement);
-console.log(days.nextElementSibling);
+// console.log(days.nextElementSibling);
+
+
+// Creating new element ----------------------------------------------------------------------
+
+const div = document.createElement("div")
+// console.log(div);
+div.className = "main";   // creating class name
+div.id = "myId";    // creating id 
+div.setAttribute("title", "generated title");  // setting attribute
+div.style.backgroundColor = "blue";   // adding styling
+div.style.padding = "25px";
+const text = document.createTextNode("Hello form js");
+div.appendChild(text);  // appendChild is adding text to div
+document.body.appendChild(div);  // showing our div to browser web page by using appendChild to body
