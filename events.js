7ec57e3f56a1bd74fe746f3bd3bb90a13fbe7a82
Tushar Negi <negitushar81@gmail.com>
2024-09-04 16:17:42 +0530

    .

--- events.js
+++ events.js
@@ -2,3 +2,14 @@
 // Events are things that happen in the system you are programming — the system produces      (or "fires") a signal of some kind when an event occurs, and provides a mechanism by which an action can be automatically taken (that is, some code running) when the event occurs. 
 
 // For example, if the user clicks a button on a webpage, you might want to react to that action by displaying an information box.
+
+document.getElementById("plant").addEventListener("click", (e)=> {
+    // alert("Plant image clicked");
+    // event object
+    // console.log(e);
+
+    // main in event object 
+    // type, timestamp, defaultPrevented, target, toElement, sourceElement, currentTarget, clientX, clientY, screenX, screenY
+})
+
+
