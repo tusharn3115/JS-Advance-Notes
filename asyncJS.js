2a33cf8d06c7fd83e3e1a3119753ec6db98880dc
Tushar Negi <negitushar81@gmail.com>
2024-09-06 10:23:38 +0530

    .

--- asyncJS.js
+++ asyncJS.js
@@ -3,3 +3,16 @@
 // By default javascript is syncronous i.e the code executes from line to line 
 // javascript is also single threaded
 // each operation waits for the last one to complete before execution
+
+
+// setTimeout() ----------------------------------------------------
+// setTimeout contains two parameters function and time(in milliseconds)
+// The global setTimeout() method sets a timer which executes a function or specified piece of code once the timer expires
+
+// The setTimeout() is executed only once
+// If you need repeated executions, use setInterval() instead.
+
+// eg:-
+setTimeout(function(){
+    console.log("Hello");
+},2000)
