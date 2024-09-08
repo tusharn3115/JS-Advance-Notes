868bb9d541fcfbccee6a892dfc6496cf6ec878c6
Tushar Negi <negitushar81@gmail.com>
2024-09-08 11:26:27 +0530

    .

---
+++ hiddenObjProperties.js
@@ -0,0 +1,23 @@
+// object getOwnPropertyDescripter --------------------------------------------------
+
+// there are some properties of object which can not be seen that easily 
+// the property getOwnPropertyDescripter tells us about some of the hidden properties of object 
+
+// Object.getOwnPropertyDescriptor(); // two parameters name and key
+
+// for example: 
+// console.log(Math.PI);
+
+// Math.PI is universal selector who's value(PI = 3.141592653589793) cannot be changed 
+// so if you will try to overwrite or change its value you will not be able to change the value, but why we cannot change the value not because its a universal selector but 
+
+const value = Object.getOwnPropertyDescriptor(Math, "PI");
+console.log(value);
+
+// output:
+// {
+//     value: 3.141592653589793,
+//     writable: false,
+//     enumerable: false,
+//     configurable: false
+// }
